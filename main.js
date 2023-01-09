f966a94a3f8f735185c927731e2f1b3b644b92b0
Jonas <78270099+jm1069@users.noreply.github.com>
2023-01-09 15:21:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const foods = [
     {name: 'Test', calories: 100, protein: 50, fat: 2.5}
 ];
+
+function addFood(){
+    const name = document.getElementById('name').value;
+    const calories = parseInt(document.getElementById('calories').value);
+    const protein = parseInt(document.getElementById('protein').value);
+    const fat = parseInt(document.getElementById('fat').value);
+
+    if (name.trim() === '' || calories <= 0 || protein <= 0 || fat <= 0) {
+        alert('Bitte gültige Werte eingeben!');
+        return;
+    }
+
+    foods.push({name: name, calories: calories, protein: protein, fat: fat});
+    console.log(foods)
+    console.log('Total', calculateTotals());
+}
